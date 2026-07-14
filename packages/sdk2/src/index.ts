24170c08492d7fd5ac5bc536eec4671bdd952f99
Wolf Mermelstein <wolf@404wolf.com>
2026-07-14 19:55:56 +0000

    remove secondary sdk

--- packages/sdk2/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { MacroClient } from './client';
-export { MacroEvents } from './events';
-export type { MacroOpts } from './config';
-export type { EventName, EventHandler, EventPayload } from './events';
